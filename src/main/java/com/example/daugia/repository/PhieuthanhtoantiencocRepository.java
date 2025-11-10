1376401d1316f215810607343dd0685d71d06cb2
nguyencongtan203 <tannguyencong203@gmail.com>
2025-11-10 23:25:39 +0700

    Cập nhật lại vài controller và có db mới với pass đều là 123456

--- src/main/java/com/example/daugia/repository/PhieuthanhtoantiencocRepository.java
+++ src/main/java/com/example/daugia/repository/PhieuthanhtoantiencocRepository.java
@@ -4,6 +4,11 @@ import com.example.daugia.entity.Phieuthanhtoantiencoc;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+import java.util.Optional;
+
 @Repository
 public interface PhieuthanhtoantiencocRepository extends JpaRepository<Phieuthanhtoantiencoc, String> {
+    List<Phieuthanhtoantiencoc> findByTaiKhoan_Matk(String matk);
+    Optional<Phieuthanhtoantiencoc> findByTaiKhoan_MatkAndPhienDauGia_Maphiendg(String matk, String maphiendg);
 }
